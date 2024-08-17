6902eeee1877a352338172995cde1f5387388464
Md Amran Hossen <mdamranhossen77@gmail.com>
2024-08-17 23:12:00 +0600

    redux added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Counter from './Counter';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/counterSlice";
 
-function App() {
+const App = () => {
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <Counter />
+    <div className="h-screen flex items-center justify-center">
+      <div className="font-serif flex border border-blue-800 p-16 gap-5">
+        <button
+          className="bg-green-700 px-3 py-2 font-semibold rounded-md text-2xl text-white"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <h2 className="text-3xl font-semibold">{count}</h2>
+        <button
+          className="bg-red-700 px-3 py-2 font-semibold rounded-md text-2xl text-white font-serif"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
